438aefc0543664f0fc1a5a11b033d498b202419c
Evash2k <75533145+Evash2k@users.noreply.github.com>
2025-11-10 09:33:23 -0500

    Update Main.py
    Added money system

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
-from IPython.display import clear_output
 import random
 import time
+
+
+
+
 
 play_game = input("Start the game? y/n:")
 
@@ -12,18 +15,31 @@ def reset_player():
 def reset_dealer():
   dealer_current_deck = 0
 
+
+current_cash = 10000
+
+
 while (play_game.lower() == "y"):
+
+  print(format(f"You currently have ${current_cash:,} "))
+  time.sleep(2)
+  betting_ammount = int(input("How much are you betting?: "))
+  while (betting_ammount > current_cash):
+    betting_ammount = int(input(f"You can't bet more than {current_cash}"))
+
+
   player_current_deck = 0
   dealer_current_deck = 0
+
+
+
   random_card_player = random.randint(1,11)
   random_card_dealer = random.randint(1,11)
 
   player_current_deck += random_card_player
-
   dealer_current_deck += random_card_dealer
 
   print(f"You have {player_current_deck} and the dealer has {dealer_current_deck} ")
-  time.sleep(2)
   hit_or_stand = input("Hit or stand: ")
 
   while(hit_or_stand.lower() == "hit"):
@@ -33,60 +49,55 @@ while (play_game.lower() == "y"):
 
     if (player_current_deck > 21):
       print(f"You have {player_current_deck} which is greater than 21!")
-      print("You lose!")
+      current_cash -= betting_ammount
+      print(format(f"You lose! You now have ${current_cash: ,} in your bank account."))
+      time.sleep(2)
       play_game = input("Restart the game? y/n:")
       break
-          
-    else: 
-      print(f"You have now have {player_current_deck}")
-      time.sleep(2)
-      hit_or_stand = input("Hit or stand: ")
 
+    elif (player_current_deck < 21):
+      print(f"You have now have {player_current_deck}")
+      hit_or_stand = input("Hit or stand: ")
+    else:
+      print(f"You now have {player_current_deck} so you automatically win!")
+      current_cash += betting_ammount
+      print(format(f"You now have ${current_cash: ,} in your bank account."))
+      play_game = input("Restart the game? y/n:")
+      break
 
 
   while (hit_or_stand.lower() == "stand"):
     random_card_dealer = random.randint(1,11)
     dealer_current_deck += random_card_dealer
-    del random_card_dealer
+
 
     if (dealer_current_deck > 21):
+      print(f"The dealer rolled {random_card_dealer}")
       print(f"The dealer has {dealer_current_deck} and busted! You win!")
-
+      current_cash += betting_ammount
+      print(format(f"You now have ${current_cash: ,} in your bank account."))
       play_game = input("Restart the game? y/n:")
       break
 
     if (dealer_current_deck >= 17 and dealer_current_deck <= 21):
 
       if (dealer_current_deck > player_current_deck):
+        print(f"The dealer rolled {random_card_dealer}")
         print(f"The dealer has {dealer_current_deck} and you have {player_current_deck}, you lose!")
+        current_cash -= betting_ammount
+        print(format(f"You now have ${current_cash: ,} in your bank account."))
         play_game = input("Restart the game? y/n:")
         break
-        
+
 
       elif (dealer_current_deck < player_current_deck):
+        print(f"The dealer rolled {random_card_dealer}")
         print(f"The dealer has {dealer_current_deck} and you have {player_current_deck}, you win!")
+        current_cash += betting_ammount
+        print(format(f"You now have ${current_cash: ,} in your bank account."))
         play_game = input("Restart the game? y/n:")
         break
-        
+
     else:
-      print(f"The dealer currently has {dealer_current_deck}")
-      time.sleep(2)
-  clear_output()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      print(f"The dealer rolled {random_card_dealer} and now has {dealer_current_deck}")
+    del random_card_dealer
